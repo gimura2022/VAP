4eb84c11346158ada616af65044b1925afff8d0c
gimura2022 <glebmuravlev0511@gmail.com>
2022-07-17 22:38:17 +0300

    VAP 0.0.3
    Добавлена команда "выключи компьютер".

--- log.py
+++ log.py
@@ -3,22 +3,21 @@ from json import load,dumps
 from os import mkdir
 
 try:
-    with open("config\\sesion.json") as f:sesion = load(f)
+    with open("log\\sesion.json") as f:sesion = load(f)
 
 except:
-    with open("config\\sesion.json","w+") as f:f.write(dumps(0));sesion = load(f)
+    try:mkdir("log")
 
-with open("config\\sesion.json","w") as f:f.write(dumps(sesion + 1))
+    except:pass
+
+    with open("log\\sesion.json","w+") as f:f.write(dumps(0));sesion = load(f)
+
+with open("log\\sesion.json","w") as f:f.write(dumps(sesion + 1))
 
 with open("config\\config.json") as config_file:config = load(config_file)
 
 def file_log(data):
     if config["loging"]:
-        try:
-            with open(f"log\\{sesion}.log","a",encoding='UTF-8') as f:f.write(f"[{datetime.now().strftime('%H:%M:%S')}]{data}\n")
-
-        except:
-            mkdir("log")
-            with open(f"log\\{sesion}.log","a",encoding='UTF-8') as f:f.write(f"[{datetime.now().strftime('%H:%M:%S')}]{data}\n")
+        with open(f"log\\{sesion}.log","a",encoding='UTF-8') as f:f.write(f"[{datetime.now().strftime('%H:%M:%S')}]{data}\n")
 
     print(f"[{datetime.now().strftime('%H:%M:%S')}]{data}")
